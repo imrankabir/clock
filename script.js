69d4eb1443072983f7cffc09b7b896acd4b60957
Imran Kabir <imran.kabir.k@gmail.com>
2024-08-15 18:26:28 +0500

    clock auto commit on 2024-08-15 06:26:28 PM

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const app = 'Clock';
+const VISITS_KEY = 'clock-visits';
+
 const hourHand = document.querySelector(".hour-hand");
 const minuteHand = document.querySelector(".minute-hand");
 const secondHand = document.querySelector(".second-hand");
 
-function setClock() {
+const setClock = e => {
   const now = new Date();
 
   const seconds = now.getSeconds();
@@ -22,13 +25,72 @@ function setClock() {
     month: "long",
     year: "numeric",
   });
+
   document.querySelector(".time").textContent = now.toLocaleString("en-US", {
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
     hour12: true,
   });
+
 }
+
+const padTwoDigits = num => num.toString().padStart(2, "0");
+
+const formatDate = (date, dateDiveder = '-') => {
+  return (
+    [
+      date.getFullYear(),
+      padTwoDigits(date.getMonth() + 1),
+      padTwoDigits(date.getDate()),
+    ].join(dateDiveder) +
+    " " +
+    [
+      padTwoDigits(date.getHours()),
+      padTwoDigits(date.getMinutes()),
+      padTwoDigits(date.getSeconds()),
+    ].join(":")
+  );
+}
+
+async function getVisitorIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+        return 'Unknown IP';
+    }
+}
+
+async function trackVisitor() {
+    const ip = await getVisitorIP();
+    const time = formatDate(new Date());
+    let visits = JSON.parse(localStorage.getItem(VISITS_KEY)) || [];
+    visits.push({ip, time, app});
+    localStorage.setItem(VISITS_KEY, JSON.stringify(visits));
+}
+
+async function persistVisits() {
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  // headers.append('mode', 'no-cors');
+  const response = await fetch('https://enabled-humpback-lively.ngrok-free.app/save-visits.php', {
+    method: 'POST',
+    body: JSON.stringify(localStorage.getItem(VISITS_KEY)),
+    headers
+  });
+
+  if (response.ok === true && response.status === 200) {
+    console.log(response);
+    localStorage.setItem(VISITS_KEY, JSON.stringify([]));
+  }
+
+}
+
+trackVisitor();
+persistVisits();
 
 setInterval(setClock, 1000);
 
