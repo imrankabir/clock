477a7252c25f0b73aa427ba4e4af0f24e08ed734
Imran Kabir <imran.kabir.k@gmail.com>
2025-06-09 17:17:40 +0500

    clock auto commit on 2025-06-09 05:17:40 PM

--- script.js
+++ script.js
@@ -20,46 +20,24 @@ const setClock = e => {
   const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-  document.querySelector(".date").textContent = now.toLocaleString("en-US", {
+  const date = document.querySelector(".date");
+  const time = document.querySelector(".time");
+
+  date.innerHTML = now.toLocaleString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
+  date.innerHTML += '<br>';
+  date.innerHTML += now.toLocaleString("en-US", {
+    weekday: "long"
+  });
 
-  document.querySelector(".time").textContent = now.toLocaleString("en-US", {
+  time.innerHTML = now.toLocaleString("en-US", {
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
     hour12: true,
-  });
-
-  // document.querySelector(".day-circle").innerHTML = `<span>${now.toLocaleString("en-US", {
-  //   weekday: "long"
-  // })}</span>`;
-
-  // const day = now.toLocaleString("en-US", { weekday: "long" });
-  // const circle = document.querySelector(".day-circle");
-  // circle.innerHTML = "";
-  // const angleStep = 360 / day.length;
-  // [...day].forEach((char, index) => {
-  //   const span = document.createElement("span");
-  //   span.innerText = char;
-  //   const angle = angleStep * index;
-  //   span.style.transform = `rotate(${angle}deg)`;
-  //   circle.appendChild(span);
-  // });
-
-  const day = now.toLocaleString("en-US", { weekday: "long" });
-  const circle = document.querySelector(".day-circle");
-  circle.innerHTML = "";
-  const radius = 40;
-  const angleStep = 360 / day.length;
-  [...day].forEach((char, index) => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    const angle = angleStep * index;
-    span.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(${-angle}deg)`;
-    circle.appendChild(span);
   });
 
 }
